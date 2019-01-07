c2e3783fc8a320cd35da864f2135b4fb479fa7a7
retiutut <retiutut@icloud.com>
2019-01-06 22:19:06 -0600

    Code Review: Cleanup v1.0.0

--- src/pages/WeatherApp.js
+++ src/pages/WeatherApp.js
@@ -10,7 +10,7 @@ import Grid from '@material-ui/core/Grid';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+//import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -45,7 +45,6 @@ class WeatherApp extends React.Component {
   }
 
   /* This life cycle hook gets executed when the component mounts */
-
   componentDidMount() {
     this.setState({
       currentTemp: 5.0,
@@ -55,22 +54,18 @@ class WeatherApp extends React.Component {
     });
   }
 
+  // Form submission logic
   handFormSubmit(e) {
-    // Form submission logic
-    //e.preventDefault();
+    
+    e.preventDefault();
     let userCity = this.state.cityName;
-    //////////////////////////////
-    //      Set Functions       //
-    //////////////////////////////
-
-    weather.setLang('en');
+    
+    //set city
     weather.setCity(userCity);
+    weather.setLang('en');
     weather.setUnits('imperial');
     weather.setAPPID('7f018bf7be7a15eae90908537e12fc3e');
 
-    //////////////////////////////
-    //      Get Functions       //
-    //////////////////////////////
     // get the Temperature  
     weather.getTemperature(function(err, temp){
       currentTemperature = temp;
@@ -98,6 +93,7 @@ class WeatherApp extends React.Component {
     });
   }
 
+  //Handles changes to text input
   handleChange(e) {
     this.setState({
         cityName: e.target.value
@@ -134,6 +130,7 @@ class WeatherApp extends React.Component {
     });
   };
 
+  //Submit form if user presses enter while using text field
   onKeyPress= (e) => {
     if (e.key === 'Enter') {
       console.log('Enter key pressed');
@@ -223,71 +220,3 @@ class WeatherApp extends React.Component {
 }
 
 export default (WeatherApp);
-
-
-/*
-var weather = require('openweather-apis');
- 
-weather.setLang('en');
-// English - en, Russian - ru, Italian - it, Spanish - es (or sp),
-// Ukrainian - uk (or ua), German - de, Portuguese - pt,Romanian - ro,
-// Polish - pl, Finnish - fi, Dutch - nl, French - fr, Bulgarian - bg,
-// Swedish - sv (or se), Chinese Tra - zh_tw, Chinese Sim - zh (or zh_cn),
-// Turkish - tr, Croatian - hr, Catalan - ca
-
-
-// set city by name
-weather.setCity('New Orleans');
- // or set the coordinates (latitude,longitude)
-//weather.setCoordinate(50.0467656, 20.0048731);
-// or set city by ID (recommended by OpenWeatherMap)
-//weather.setCityId(4367872);
-// or set zip code
-//weather.setZipCode(33615);
-
-// 'metric'  'internal'  'imperial'
- weather.setUnits('imperial');
-
-// check http://openweathermap.org/appid#get for get the APPID
- weather.setAPPID('7f018bf7be7a15eae90908537e12fc3e');
-
-//////////////////////////////
-//      Get Functions       //
-//////////////////////////////
-
- // get the Temperature  
- weather.getTemperature(function(err, temp){
-    console.log(temp);
-});
-
-// get the Atm Pressure
-weather.getPressure(function(err, pres){
-    console.log(pres);
-});
-
-// get the Humidity
-weather.getHumidity(function(err, hum){
-    console.log(hum);
-});
-
-// get the Description of the weather condition
-weather.getDescription(function(err, desc){
-    console.log(desc);
-});
-*/
-
-
-/*
-fetch('http://example.com',{
-    method: "POST",
-    body: JSON.stringify(userData),
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-  }).then(response => {
-    response.json().then(data =>{
-      console.log("Successful" + data);
-    })
-});
-*/
---
+++ src/pages/defaultFunctions.js
@@ -0,0 +1,66 @@
+/*
+var weather = require('openweather-apis');
+ 
+weather.setLang('en');
+// English - en, Russian - ru, Italian - it, Spanish - es (or sp),
+// Ukrainian - uk (or ua), German - de, Portuguese - pt,Romanian - ro,
+// Polish - pl, Finnish - fi, Dutch - nl, French - fr, Bulgarian - bg,
+// Swedish - sv (or se), Chinese Tra - zh_tw, Chinese Sim - zh (or zh_cn),
+// Turkish - tr, Croatian - hr, Catalan - ca
+
+
+// set city by name
+weather.setCity('New Orleans');
+ // or set the coordinates (latitude,longitude)
+//weather.setCoordinate(50.0467656, 20.0048731);
+// or set city by ID (recommended by OpenWeatherMap)
+//weather.setCityId(4367872);
+// or set zip code
+//weather.setZipCode(33615);
+
+// 'metric'  'internal'  'imperial'
+ weather.setUnits('imperial');
+
+// check http://openweathermap.org/appid#get for get the APPID
+ weather.setAPPID('7f018bf7be7a15eae90908537e12fc3e');
+
+//////////////////////////////
+//      Get Functions       //
+//////////////////////////////
+
+ // get the Temperature  
+ weather.getTemperature(function(err, temp){
+    console.log(temp);
+});
+
+// get the Atm Pressure
+weather.getPressure(function(err, pres){
+    console.log(pres);
+});
+
+// get the Humidity
+weather.getHumidity(function(err, hum){
+    console.log(hum);
+});
+
+// get the Description of the weather condition
+weather.getDescription(function(err, desc){
+    console.log(desc);
+});
+*/
+
+
+/*
+fetch('http://example.com',{
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  }).then(response => {
+    response.json().then(data =>{
+      console.log("Successful" + data);
+    })
+});
+*/
